99197fcb4081120205fd651467e90917f49b8796
assidik12 <sofi.sidik12@gmail.com>
2025-08-14 11:17:16 +0700

    fix url api-specs

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: true, limit: "2mb" })); // Limit URL-enco
 
 dbconfig.connect();
 
-const swaggerDocument = YAML.load(path.join(__dirname, "./src/docs/swagger.yaml"));
+const swaggerDocument = YAML.load(path.join(__dirname, "./docs/swagger.yaml"));
 
 // Buat route untuk dokumentasi API
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
